870591a65d26a3dacd889e266cd462d4af520619
Vishwajeet Shinde <shindevishwajeet434@gmail.com>
2024-10-21 18:21:40 +0530

    removed error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import GraphVisualizer from "./components/GraphVisualizer";
+import PredictComponent from './components/PredictComponent';
 
 import Monitor from "./components/Monitor";
 import FAQ from "./components/faq"; // Import the FAQ componentimport PredictComponent from "./components/PredictComponent";
@@ -13,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/visualize" element={<GraphVisualizer />} />
-          <Route path="/predict" element={<PredictionForm />} />
+          <Route path="/predict" element={<PredictComponent />} />
           <Route path="/monitor" element={<Monitor />} />
           <Route path="/faq" element={<FAQ />} /> {/* Added FAQ Route */}
         </Routes>
